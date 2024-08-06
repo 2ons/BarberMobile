0d98587c40490ef2b20cc17eeec52295b1a58b32
Nimer Shadida Johansson <nimer.shadida.johansson@gmail.com>
2024-08-06 22:24:30 +0200

    added wrangler

---
+++ functions/_workers.js
@@ -0,0 +1,22 @@
+// functions/_worker.js
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event));
+});
+
+async function handleRequest(event) {
+  try {
+    const page = await getAssetFromKV(event, {
+      mapRequestToAsset: req => {
+        return mapRequestToAsset(req);
+      }
+    });
+    return new Response(page.body, page);
+  } catch (e) {
+    if (e.status === 404) {
+      return new Response('Not Found', { status: 404 });
+    }
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
